aa0635216824088552591afe0349b80f2fb43a24
Isaac Stalley <isaacstalley@gmail.com>
2023-09-16 17:59:38 -0600

    Firmware finished

--- src/stop_lights.c
+++ src/stop_lights.c
@@ -9,11 +9,21 @@
 #define CROSSWALK_BLINK_STATE 4
 #define TRANSITION_TO_TRAFFIC_STATE 5
 
+// Output Pins (LEDS)
+#define TRAFFIC_GREEN 1 // PB1
+#define TRAFFIC_RED 2 // PB2
+#define CROSSWALK_GREEN 3 //PB3
+#define CROSSWALK_GREEN2 4 //PB4
+#define CROSSWALK_RED 5 // PB5
+#define CROSSWALK_RED2 6 // PB6
+
+#define BLINK_COUNTER 6 // Number of LED blinks
+
 typedef unsigned int uint;
 
 volatile uint buttonPressed = 0; // Flag to indicate button press
 volatile uint timerCounter = 0; // Timer counter to track time
-
+uint trafficMinimumMet = 0;
 uint current_state = TRAFFIC_GREEN_STATE;
 
 ISR(INT0_vect) { // INTO button interrupt
@@ -34,22 +44,55 @@ void updateStateMachine() {
     switch (current_state) {
         // TRAFFIC_GREEN_STATE 
          case TRAFFIC_GREEN_STATE:
+            PORTB &= ~(1 << TRAFFIC_RED); // Turn off 
+            PORTB |= (1 << TRAFFIC_GREEN) | (1 << CROSSWALK_RED) | (1 << CROSSWALK_RED2); // Turn on
+            if (!trafficMinimumMet) { // Check for minimum time met of 10s
+                delay(10);
+                trafficMinimumMet = 1; // Minimum time met flag
+            }
+            else if (buttonPressed) { // If minumum time met and button pressed
+                current_state = TRAFFIC_BLINK_STATE;
+                buttonPressed = 0;
+                trafficMinimumMet = 0;
+            }
             break;
         // TRAFFIC_BLINK_STATE
         case TRAFFIC_BLINK_STATE:
+            for (int i = 0; i < BLINK_COUNTER; i++) { // For loop to blink green traffic light
+                PORTB ^= (1 << TRAFFIC_GREEN); // Toggle green traffic light
+                delay(0.5);
+            }
+            current_state = TRANSITION_TO_CROSSWALK_STATE;
             break;
         // TRANSITION_TO_CROSSWALK_STATE
         case TRANSITION_TO_CROSSWALK_STATE:
+            PORTB &= ~(1 << TRAFFIC_GREEN); // Turn off
+            PORTB |= (1 << TRAFFIC_RED); // Turn on
+            delay(1);
+            current_state = CROSSWALK_GREEN_STATE;
             break;
         // CROSSWALK_GREEN_STATE
         case CROSSWALK_GREEN_STATE:
+            buttonPressed = 0;
+            PORTB &= ~((1 << CROSSWALK_RED) | (1 << CROSSWALK_RED2)); // Turn off
+            PORTB |= (1 << CROSSWALK_GREEN) | (1 << CROSSWALK_GREEN2); // Turn on
+            delay(10);
+            current_state = CROSSWALK_BLINK_STATE;
             break;
         // CROSSWALK_BLINK_STATE
         case CROSSWALK_BLINK_STATE:
+                for (int i = 0; i < BLINK_COUNTER; i++) { // Loop to blink crosswalk green light
+                    PORTB ^= (1 << CROSSWALK_GREEN) | (1 << CROSSWALK_GREEN2); // Toggle
+                    delay(0.5);
+                }
+            current_state = TRANSITION_TO_TRAFFIC_STATE;
             break;
         // TRANSITION_TO_TRAFFIC_STATE
         case TRANSITION_TO_TRAFFIC_STATE:
-
+            PORTB &= ~((1 << CROSSWALK_GREEN) | (1 << CROSSWALK_GREEN2)); // Turn off
+            PORTB |= (1 << CROSSWALK_RED) | (1 << CROSSWALK_RED2); // Turn on
+            delay(1);
+            current_state = TRAFFIC_GREEN_STATE;
             break;
     }
 }
@@ -63,6 +106,10 @@ int main() {
     TIMSK |= (1 << OCIE0A); // Enable timer compare interrupt
     TCCR0B |= (1 << CS02) | (1 << CS00); // Start timer with prescaler 1024
     OCR0A = 255; // Set compare match value for desired delay
+
+    DDRD &= ~(1 << PD2); // Set D2 as input pin
+    DDRB |= (1 << TRAFFIC_GREEN) | (1 << TRAFFIC_RED) | (1 << CROSSWALK_GREEN) | (1 << CROSSWALK_GREEN2) | (1 << CROSSWALK_RED) | (1 << CROSSWALK_RED2); // Set pins as outputs
+    PORTB = 0;
 
     while (1) {
         updateStateMachine();
